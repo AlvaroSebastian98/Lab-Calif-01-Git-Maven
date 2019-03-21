1826821046bfa6140c7f47e61e6d043514faccec
Alumno <Alumno@LAB1507-22>
2019-03-21 13:01:41 -0500

    Segundo Commit

--- src/main/java/com/tecsup/lab01/model/User.java
+++ src/main/java/com/tecsup/lab01/model/User.java
@@ -1,5 +1,13 @@
 package com.tecsup.lab01.model;
 
-public class User {
 
+
+public class User {
+	
+	public String nombres = "Juanita";
+	public String apellidoP = "Cordova";
+	public String apellidoM = "Perez";
+	public int edad = 18;
+	
+	
 }
