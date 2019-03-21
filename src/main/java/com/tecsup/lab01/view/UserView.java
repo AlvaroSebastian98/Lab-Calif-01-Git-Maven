aafa21bdbf594de7a7f30928d33bf8fa8c482e29
pierodead <delpiee@hotmail.com>
2019-03-21 13:35:21 -0500

    commit view2

--- src/main/java/com/tecsup/lab01/view/UserView.java
+++ src/main/java/com/tecsup/lab01/view/UserView.java
@@ -1,5 +1,30 @@
 package com.tecsup.lab01.view;
+import com.tecsup.lab01.controller.*;
 
+import com.tecsup.lab01.controller.*;
 public class UserView {
-
+	
+		UserController u = new UserController();
+		String nc= u.getNombreCompleto();
+		
+		public void imprimir() {
+			System.out.println(nc);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+	
 }
